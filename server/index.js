8f9d83155f12fd55589ee378edd1e9dddc7861fa
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2024-01-15 20:23:17 -0500

    Route and controller folders added

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
-const port = 5000; 
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require('mongoose');
+const port = process.env.PORT || 5000; 
 
 // middleware
 app.use(express.json());
 
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connected to the Database")
+      } catch (error) {
+        console.error("Error connecting to Database", error);
+        process.exit(1)
+      }
+}
+
+
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`)
+    console.log(`Server is listening on http://localhost:${port}`)
 })
+
+connectDB();
