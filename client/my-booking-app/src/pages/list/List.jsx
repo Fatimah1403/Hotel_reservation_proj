c4e3e3e2675ad5eb193a9e478f9b46667dea036b
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2024-01-21 23:08:57 -0500

    updated some components

--- client/my-booking-app/src/pages/list/List.jsx
+++ client/my-booking-app/src/pages/list/List.jsx
@@ -5,6 +5,7 @@ import "./List.css"
 import { useLocation } from 'react-router-dom'
 import { format } from "date-fns";
 import { DateRange } from "react-date-range";
+import SearchItem from '../../components/searchItem/SearchItem'
 
 const List = () => {
 
@@ -93,7 +94,17 @@ const List = () => {
             </div>
             <button>Search</button>
           </div>
-          <div className="listResult"></div>
+          <div className="listResult">
+          <     SearchItem/>
+                <SearchItem/>
+                <SearchItem/>
+                <SearchItem/>
+                <SearchItem/>
+                <SearchItem/>
+                <SearchItem/>
+                <SearchItem/>
+                <SearchItem/>
+          </div>
                 
         </div>
       </div>
